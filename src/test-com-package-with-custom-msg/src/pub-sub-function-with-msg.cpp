c55238cab383c76b0c935c3cfc8237f3ced00ea5
gcy0115 <gcy15256266892@163.com>
2024-12-04 16:59:10 +0800

    完成四个转向电机的自定义消息发送，下一步使用实际电机测试

--- src/test-com-package-with-custom-msg/src/pub-sub-function-with-msg.cpp
+++ src/test-com-package-with-custom-msg/src/pub-sub-function-with-msg.cpp
@@ -6,6 +6,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 #include "custom_interfaces/msg/angle.hpp"
+#include "custom_interfaces/msg/steering_motors.hpp"
 
 using namespace std::chrono_literals;
 using std::placeholders::_1;
@@ -21,7 +22,7 @@ class MinimalPublisher : public rclcpp::Node
     调用父类构造函数，初始化节点，指定名称为 "minimal_publisher"。
     初始化 count_ 为 0，用于消息计数。*/
     {
-      publisher_ = this->create_publisher<custom_interfaces::msg::Angle>("message", 10);
+      publisher_ = this->create_publisher<custom_interfaces::msg::SteeringMotors>("steering_state_message", 10);
 
       timer_ = this->create_wall_timer(500ms, std::bind(&MinimalPublisher::timer_callback, this));
       /*
@@ -32,14 +33,47 @@ class MinimalPublisher : public rclcpp::Node
   private:
     void timer_callback()
     {
-      auto message = custom_interfaces::msg::Angle();
-      // 创建一个消息对象，类型为 std_msgs::msg::String，用来存储即将发布的数据。
-      message.angle = count_++;
-      /*
-      设置消息的 data 字段。
-      count_++ 是一个计数器，用于在每条消息后附加递增的数字。*/
+      auto message = custom_interfaces::msg::SteeringMotors();
+      // 创建一个消息对象，类型为 custom_interfaces::msg::SteeringMotors()，用来存储即将发布的数据。
       
-      RCLCPP_INFO(this->get_logger(), "Publishing: '%ld'", message.angle);
+      // 填充motor1的数据，实际内容可以使用电机反馈帧
+      message.motor1.position = 1.0;
+      message.motor1.velocity = 0.5;
+      message.motor1.torque = 0.2;
+      message.motor1.temperature = 40.0;
+      message.motor1.err = 1;
+      message.motor1.canid = 0x101;
+      message.motor1.name = "FL";
+
+      // 填充motor2的数据，实际内容可以使用电机反馈帧
+      message.motor2.position = 2.0;
+      message.motor2.velocity = 1.5;
+      message.motor2.torque = 1.2;
+      message.motor2.temperature = 40.0;
+      message.motor2.err = 1;
+      message.motor2.canid = 0x102;
+      message.motor2.name = "FR";
+
+      // 填充motor3的数据，实际内容可以使用电机反馈帧
+      message.motor3.position = 3.0;
+      message.motor3.velocity = 2.5;
+      message.motor3.torque = 1.6;
+      message.motor3.temperature = 42.0;
+      message.motor3.err = 1;
+      message.motor3.canid = 0x103;
+      message.motor3.name = "BR";
+
+      // 填充motor4的数据，实际内容可以使用电机反馈帧
+      message.motor4.position = 4.0;
+      message.motor4.velocity = 4.5;
+      message.motor4.torque = 1.9;
+      message.motor4.temperature = 43.0;
+      message.motor4.err = 1;
+      message.motor4.canid = 0x104;
+      message.motor4.name = "BL";
+
+      
+      RCLCPP_INFO(this->get_logger(), "Publishing steering motors\' states");
       /*
       使用 ROS 日志系统打印发布的消息内容。
       this->get_logger() 返回该节点的日志记录器*/
@@ -47,7 +81,7 @@ class MinimalPublisher : public rclcpp::Node
       // 将创建的消息发布到话题 message。
     }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<custom_interfaces::msg::Angle>::SharedPtr publisher_;
+    rclcpp::Publisher<custom_interfaces::msg::SteeringMotors>::SharedPtr publisher_;
     size_t count_;
     /*
     rclcpp::TimerBase::SharedPtr timer_：指向定时器的智能指针，用于触发 timer_callback。
@@ -67,42 +101,30 @@ class MinimalSubscriber : public rclcpp::Node
     调用父类构造函数，初始化节点，指定名称为 "minimal_subscriber"。
     */
     {
-      subscription_ = this->create_subscription<custom_interfaces::msg::Angle>(
-      "message", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-    /*
-    功能：
-    1. 创建一个订阅者，订阅 ROS 2 话题 message。
-    2. 订阅的消息类型是 std_msgs::msg::String。
-    3. 消息队列大小为 10，表示最多可以缓存 10 条未处理的消息。
-    4. 当有新消息到达时，会调用 topic_callback 函数处理消息。
-
-    关键点：
-    std::bind(&MinimalSubscriber::topic_callback, this, _1)：
-      1.将成员函数 topic_callback 绑定到当前类的实例。
-      2. this 指向当前对象（MinimalSubscriber 的实例），确保 topic_callback 在正确的上下文中执行。
-      3. _1 表示占位符，用于接收订阅到的消息。
-    */
+      subscription_ = this->create_subscription<custom_interfaces::msg::SteeringMotors>(
+      "steering_state_message", 10, std::bind(&MinimalSubscriber::topic_callback, this, std::placeholders::_1));
     }
 
 
   private:
-    void topic_callback(const custom_interfaces::msg::Angle & msg) const
+    void topic_callback(const custom_interfaces::msg::SteeringMotors::SharedPtr msg)
     {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%ld'", msg.angle);
-    }
-    /*
-    功能
-    参数：
-      const std_msgs::msg::String & msg：引用类型参数，表示接收到的 ROS 2 消息。
-      消息内容存储在 msg.data 中，是一个 std::string。
-    
-    打印消息
-      使用 ROS 2 的日志系统 RCLCPP_INFO 将接收到的消息内容打印到终端。
-      this->get_logger()：返回该节点的日志记录器。
-      msg.data.c_str()：将 std::string 转换为 C 风格字符串，方便格式化输出。
-    */
+      RCLCPP_INFO(this->get_logger(), "Received motor states:");
+        
+      RCLCPP_INFO(this->get_logger(), "Received from: motor1 - Position: %.2f, Velocity: %.2f, Torque: %.2f, Temperature: %.2f",
+                    msg->motor1.position, msg->motor1.velocity, msg->motor1.torque, msg->motor1.temperature);
 
-    rclcpp::Subscription<custom_interfaces::msg::Angle>::SharedPtr subscription_;
+      RCLCPP_INFO(this->get_logger(), "Received from: motor2 - Position: %.2f, Velocity: %.2f, Torque: %.2f, Temperature: %.2f",
+                    msg->motor2.position, msg->motor2.velocity, msg->motor2.torque, msg->motor2.temperature);
+
+      RCLCPP_INFO(this->get_logger(), "Received from: motor3 - Position: %.2f, Velocity: %.2f, Torque: %.2f, Temperature: %.2f",
+                    msg->motor3.position, msg->motor3.velocity, msg->motor3.torque, msg->motor3.temperature);
+
+      RCLCPP_INFO(this->get_logger(), "Received from: motor4 - Position: %.2f, Velocity: %.2f, Torque: %.2f, Temperature: %.2f",
+                    msg->motor4.position, msg->motor4.velocity, msg->motor4.torque, msg->motor4.temperature);
+    }
+
+    rclcpp::Subscription<custom_interfaces::msg::SteeringMotors>::SharedPtr subscription_;
     // rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_：指向订阅的智能指针，用于接收消息。
 };
 
